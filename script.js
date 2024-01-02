e8c35f9de54d6585860f2fe6e44b53f1499611f7
JegorJakovlev <yegor.yakovlev.05@gmail.com>
2024-01-02 23:25:30 +0200

    исправлена серая линия чтобы была по центру индикатора, была при переключени индикатор по центру.

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 function openTab(tabName) {
-    const tabs = document.getElementsByClassName("tabcontent");
-    for (let i = 0; i < tabs.length; i++) {
-      tabs[i].classList.remove("show");
-    }
-  
-    const buttons = document.getElementsByClassName("tab");
-    for (let i = 0; i < buttons.length; i++) {
-      buttons[i].classList.remove("selected");
-    }
-  
-    const tabContent = document.getElementById(tabName);
-    const tabButton = document.querySelector(`button[onclick="openTab('${tabName}')"]`);
-  
-    tabContent.classList.add("show");
-    tabButton.classList.add("selected");
-  
-    const indicator = document.querySelector(".indicator");
-    indicator.style.width = tabButton.offsetWidth + "px";
-    indicator.style.marginLeft = tabButton.offsetLeft + "px";
+  const tabs = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].classList.remove("show");
   }
+
+  const buttons = document.getElementsByClassName("tab");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("selected");
+  }
+
+  const tabContent = document.getElementById(tabName);
+  const tabButton = document.querySelector(`button[onclick="openTab('${tabName}')"]`);
+
+  tabContent.classList.add("show");
+  tabButton.classList.add("selected");
+
+  const indicator = document.querySelector(".indicator");
+  indicator.style.width = tabButton.offsetWidth + "px";
+  indicator.style.marginLeft = tabButton.offsetLeft + tabButton.offsetWidth / 4 - indicator.offsetWidth / 2 + "px";
+
+  const tabsContainer = document.querySelector(".tabs");
+  const persistentLine = document.createElement("div");
+  persistentLine.classList.add("persistent-line");
+  tabsContainer.appendChild(persistentLine);
   
+   
+  
+}
